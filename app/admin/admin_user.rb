4f67e53a83fd9c511c59a83e51b27521a70840c2
Paul <dotnet.gru@gmail.com>
2014-11-09 10:06:44 +0100

    active admin installed

---
+++ app/admin/admin_user.rb
@@ -0,0 +1,34 @@
+ActiveAdmin.register AdminUser do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  index do
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    # default_actions
+    actions
+  end
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :email
+    end
+    f.actions
+  end
+
+
+end
